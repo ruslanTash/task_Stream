d591baf800287cbf38ca2b8ff3cd569e08f8d4d2
ruslanTash <ruslan--t@yandex.ru>
2023-03-17 00:12:18 +0400

    Решена задача #2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,32 @@
+import java.util.*;
+import java.util.stream.Collectors;
+
+
 public class Main {
+//    public static void findMinMax(Stream <? extends T> stream,
+//                                  Comparator <? super T> order,
+//                                  BiConsumer <? super T, ? super T> minMaxConsumer){
+//    //находить в стриме минимальный и максимальный элементы в соответствии с порядком, заданным Comparator'ом
+//        minMaxConsumer.accept(min, max);
+//        minMaxConsumer.accept(null, null);
+//    }
+
+
+    public static void printEvenNumbers(Collection<Integer> integerList) {
+//Метод принимает на вход список целых чисел, определяет в списке количество четных чисел и выводит их в консоль.
+        List<Integer> evenInt = integerList.stream()
+                .filter(Objects::nonNull)
+                .filter(n -> n % 2 == 0)
+                .collect(Collectors.toList());
+        System.out.println("Количество четных чисел в списке: " + evenInt.size());
+        System.out.println(evenInt);
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        List<Integer> integerList = Arrays.asList(1, 2, 3, 4, 5, 6, 0, 9, 4, null);
+        printEvenNumbers(integerList);
+
+
     }
 }
